964c194d2d28d3819c225ecfc0c69a442a918260
Luítame de Oliveira <luitamy@hotmail.com>
2016-11-26 07:34:39 +0000

    paginacao ok

--- resources/views/visor_pontuacaos/index.blade.php
+++ resources/views/visor_pontuacaos/index.blade.php
@@ -4,7 +4,8 @@
     <section class="content-header">
         <h1 class="pull-left">Visor de Pontuação</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('visorPontuacaos.create') !!}">Adicionar novo</a>
+            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px"
+               href="{!! route('visorPontuacaos.create') !!}">Adicionar novo</a>
         </h1>
     </section>
     <div class="content">
@@ -15,7 +16,11 @@
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
-                    @include('visor_pontuacaos.table')
+                @include('visor_pontuacaos.table')
+                <hr>
+                <div class="pull-right">
+                    {!! $visorPontuacaos->render() !!}
+                </div>
             </div>
         </div>
     </div>
