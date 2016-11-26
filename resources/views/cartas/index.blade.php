fbef1327b25ded21e302a966196c94ddf98914f2
Luítame de Oliveira <luitamy@hotmail.com>
2016-11-26 20:28:13 +0000

    exibindo cartas em grid e removendo botao de adicionar nova carta

--- resources/views/cartas/index.blade.php
+++ resources/views/cartas/index.blade.php
@@ -3,9 +3,6 @@
 @section('content')
     <section class="content-header">
         <h1 class="pull-left">Cartas</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('cartas.create') !!}">Adicionar novo</a>
-        </h1>
     </section>
     <div class="content">
         <div class="clearfix"></div>
@@ -15,9 +12,8 @@
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
-                    @include('cartas.table')
+                    @include('cartas.grid')
             </div>
         </div>
     </div>
 @endsection
-
