855abb126ba2ed1ad46a7dd3a4f5f3a0055f72cb
Luítame de Oliveira <luitamy@hotmail.com>
2016-11-26 20:39:35 -0300

    exibicao em grid da cartas

--- resources/views/cartas/show_fields.blade.php
+++ resources/views/cartas/show_fields.blade.php
@@ -1,8 +1,15 @@
+<!-- Imagem -->
+<div class="row">
+    <div class="col-sm-3">
+        <img src="{{url('images/cartas/'.$carta->nome.'.png')}}" alt="{{$carta->nome}}" style="border: 1px solid #ccc;">
+    </div>
+</div>
+
 <!-- Id Field -->
 <div class="row">
     <div class="col-sm-12">
         <div class="form-group">
-            {!! Form::label('id', 'Id:') !!}
+            {!! Form::label('id', 'ID:') !!}
             <p>{!! $carta->id !!}</p>
         </div>
     </div>
@@ -14,6 +21,16 @@
         <div class="form-group">
             {!! Form::label('nome', 'Nome:') !!}
             <p>{!! $carta->nome !!}</p>
+        </div>
+    </div>
+</div>
+
+<!-- Nome Field -->
+<div class="row">
+    <div class="col-sm-12">
+        <div class="form-group">
+            {!! Form::label('numero', 'Número:') !!}
+            <p>{!! $carta->numero !!}</p>
         </div>
     </div>
 </div>
@@ -37,34 +54,3 @@
         </div>
     </div>
 </div>
-
-<!-- Verso Field -->
-<div class="row">
-    <div class="col-sm-12">
-        <div class="form-group">
-            {!! Form::label('verso', 'Verso:') !!}
-            <p>{!! $carta->verso !!}</p>
-        </div>
-    </div>
-</div>
-
-<!-- Created At Field -->
-<div class="row">
-    <div class="col-sm-12">
-        <div class="form-group">
-            {!! Form::label('created_at', 'Created At:') !!}
-            <p>{!! $carta->created_at !!}</p>
-        </div>
-    </div>
-</div>
-
-<!-- Updated At Field -->
-<div class="row">
-    <div class="col-sm-12">
-        <div class="form-group">
-            {!! Form::label('updated_at', 'Updated At:') !!}
-            <p>{!! $carta->updated_at !!}</p>
-        </div>
-    </div>
-</div>
-
