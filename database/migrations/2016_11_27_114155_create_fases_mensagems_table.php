f7bc17dc77479ff218375044934ba1fc41eb5dcc
Luítame de Oliveira <luitamy@hotmail.com>
2016-11-27 09:08:17 -0300

    migration para relacionamento entre fase e mensagems

---
+++ database/migrations/2016_11_27_114155_create_fases_mensagems_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFasesMensagemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fases_mensagems', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('fase_id');
+            $table->foreign('fase_id')->references('id')->on('fases');
+            $table->unsignedInteger('mensagem_id');
+            $table->foreign('mensagem_id')->references('id')->on('mensagems');
+            $table->unsignedSmallInteger('pontos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fases_mensagems');
+    }
+}
