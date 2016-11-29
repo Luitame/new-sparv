939eb278b609d2256d8341b23226a0252c516330
Luítame de Oliveira <luitamy@hotmail.com>
2016-11-29 02:17:39 +0000

    relacionamento com mensagems

---
+++ database/migrations/2016_11_29_021656_create_regra_extras_mensagems_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegraExtrasMensagemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regra_extras_mensagems', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('regra_extra_id');
+            $table->foreign('regra_extra_id')->references('id')->on('regra_extras');
+            $table->unsignedInteger('mensagem_id');
+            $table->foreign('mensagem_id')->references('id')->on('mensagems');
+            $table->string('ordem');
+            $table->unsignedSmallInteger('pontos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regra_extras_mensagems');
+    }
+}
