160cdb4575c168095a8bfbbaaa901b7e76bf5e3e
Luítame de Oliveira <luitamy@hotmail.com>
2016-11-29 02:16:39 +0000

    relacionamento com perguntas

---
+++ database/migrations/2016_11_29_021520_create_regra_extras_perguntas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegraExtrasPerguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regra_extras_perguntas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('regra_extra_id');
+            $table->foreign('regra_extra_id')->references('id')->on('regra_extras');
+            $table->unsignedInteger('pergunta_id');
+            $table->foreign('pergunta_id')->references('id')->on('perguntas');
+            $table->string('ordem');
+            $table->unsignedSmallInteger('pontos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regra_extras_perguntas');
+    }
+}
