b3b870c0bf76dba014d503b9f7f8d7cdbb49bb21
Luítame de Oliveira <luitamy@hotmail.com>
2016-12-03 08:51:04 +0000

    comecando a estruturar o front da criacao de uma fase

--- resources/views/fases/fields.blade.php
+++ resources/views/fases/fields.blade.php
@@ -1,15 +1,25 @@
+<!-- Carta Field -->
 <div class="row">
-    <!-- Criterio Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('criterio', 'Criterio:') !!}
-    {!! Form::select('criterio', ], null, ['class' => 'form-control']) !!}
+    <div class="form-group col-sm-2">
+        {!! Form::label('carta', 'Carta:') !!}
+        {!! Form::select('carta_id', [], null, ['class' => 'form-control']) !!}
+    </div>
+</div>
+
+<!-- Criterio Field -->
+<div class="row">
+    <div class="form-group col-sm-2">
+        {!! Form::label('criterio', 'Criterio:') !!}
+        {!! Form::select('criterio', [], null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
 <!-- Pontos Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('pontos', 'Pontos:') !!}
-    {!! Form::text('pontos', null, ['class' => 'form-control']) !!}
-</div>
+<div class="row">
+    <div class="form-group col-sm-1">
+        {!! Form::label('pontos', 'Pontos:') !!}
+        {!! Form::text('pontos', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
 <!-- Submit Field -->
