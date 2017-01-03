1ca5a52b75b28d215e033c9c518fa6303c773ecf
Luítame de Oliveira <luitamy@hotmail.com>
2017-01-03 05:27:00 -0300

    ajustando visualizacao dos timestamps

--- resources/views/regra_extras/show_fields.blade.php
+++ resources/views/regra_extras/show_fields.blade.php
@@ -1,10 +1,13 @@
-<!-- Id Field -->
 <div class="row">
     <div class="col-sm-12">
-        <div class="form-group">
-            {!! Form::label('id', 'Id:') !!}
-            <p>{!! $regraExtra->id !!}</p>
-        </div>
+        <p class="pull-right">
+            <em style="font-size: 0.7em;">
+                <strong>Criado em:</strong>
+                <span>{!! $regraExtra->created_at->format('H:m:i d/m/Y') !!} </span>
+                <strong>Ultima atualização:</strong>
+                <span>{!! $regraExtra->updated_at->format('H:m:i d/m/Y') !!} </span>
+            </em>
+        </p>
     </div>
 </div>
 
@@ -44,26 +47,6 @@
         <div class="form-group">
             {!! Form::label('pontos', 'Pontos:') !!}
             <p>{!! $regraExtra->pontos !!}</p>
-        </div>
-    </div>
-</div>
-
-<!-- Created At Field -->
-<div class="row">
-    <div class="col-sm-12">
-        <div class="form-group">
-            {!! Form::label('created_at', 'Created At:') !!}
-            <p>{!! $regraExtra->created_at !!}</p>
-        </div>
-    </div>
-</div>
-
-<!-- Updated At Field -->
-<div class="row">
-    <div class="col-sm-12">
-        <div class="form-group">
-            {!! Form::label('updated_at', 'Updated At:') !!}
-            <p>{!! $regraExtra->updated_at !!}</p>
         </div>
     </div>
 </div>
