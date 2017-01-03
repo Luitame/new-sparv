ea54baff98a60938422c3b2c76f67ec976ed0d5e
Luítame de Oliveira <luitamy@hotmail.com>
2017-01-03 05:24:29 -0300

    mostrando id da regra no titulo da pagina

--- resources/views/regra_extras/show.blade.php
+++ resources/views/regra_extras/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Regra Extra
+            Regra Extra #{{$regraExtra->id}}
         </h1>
     </section>
     <div class="content">
