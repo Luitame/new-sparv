fef1b993cd2c0ad9659a02041c4b82a076a1474d
Luítame de Oliveira <luitamy@hotmail.com>
2017-01-16 11:38:54 -0300

    mudando layout construido com box

--- resources/views/modelos/create.blade.php
+++ resources/views/modelos/create.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
-
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css">
+@endsection
 @section('content')
     <section class="content-header">
         <h1>
@@ -8,19 +10,65 @@
     </section>
     <div class="content">
         @include('adminlte-templates::common.errors')
-        <div class="box box-primary">
 
-            <div class="box-body">
-                <div class="row">
-                    <div class="col-sm-12">
-                        {!! Form::open(['route' => 'modelos.store']) !!}
+        <div class="row">
+            <div class="col-sm-12">
+                {!! Form::open(['route' => 'modelos.store']) !!}
 
-                            @include('modelos.fields')
+                @include('modelos.fields')
 
-                        {!! Form::close() !!}
-                    </div>
-                </div>
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
+@endsection
+@section('scripts')
+    <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            const urlApi = "//sparv.dev/api/";
+            const urlTranslate = "http://cdn.datatables.net/plug-ins/1.10.13/i18n/Portuguese-Brasil.json";
+
+            $('#instrucoesIniciais').DataTable({
+                "processing": true,
+                "ajax": urlApi + "instrucao_inicials",
+                "columns": [
+                    {"data": "id"},
+                    {"data": "instrucaoTxt"}
+                ],
+                "language": {
+                    "url": urlTranslate
+                }
+            });
+
+            $('#tempo').DataTable({
+                "processing": true,
+                "ajax": urlApi + "tempos",
+                "columns": [
+                    {"data": "id"},
+                    {"data": "total"},
+                    {"data": "ordem"},
+                    {"data": "mudandoEm"},
+                    {"data": "visibilidadeInicial"}
+                ],
+                "language": {
+                    "url": urlTranslate
+                }
+            });
+
+            $('#visorPontuacao').DataTable({
+                "processing": true,
+                "ajax": urlApi + "tempos",
+                "columns": [
+                    {"data": "id"},
+                    {"data": "visibilidadeInicial"},
+                    {"data": "mudandoEm"}
+                ],
+                "language": {
+                    "url": urlTranslate
+                }
+            });
+        });
+    </script>
 @endsection
