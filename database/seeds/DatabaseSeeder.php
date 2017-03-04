ef970826d68e0612b9bacdec013eb813f8d8a953
Luítame de Oliveira <luitamy@hotmail.com>
2017-03-04 19:50:11 +0000

    remomvendo comentário da seed fase

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
          $this->call(TempoSeeder::class);
          $this->call(VisorPontuacaoSeender::class);
          $this->call(CartaSeeder::class);
-//         $this->call(FaseSeeder::class);
+         $this->call(FaseSeeder::class);
 //         $this->call(RegraExtraSeeder::class);
 //         $this->call(ModeloSeeder::class);
 //         $this->call(ModeloInstrucaoInicialSeeder::class);
