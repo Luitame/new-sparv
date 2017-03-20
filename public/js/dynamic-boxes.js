e3b04c249afa2b0aae61d4bc8dfff6807691fd75
Luítame de Oliveira <luitamy@hotmail.com>
2017-03-20 20:08:46 -0300

    funcão de autocomplete

--- public/js/dynamic-boxes.js
+++ public/js/dynamic-boxes.js
@@ -1,81 +1,99 @@
 $(function () {
 
-    // note plugins setup
-    $.noty.defaults.theme = 'bootstrapTheme';
-    $.noty.defaults.timeout = 5000;
-    $.noty.defaults.progressBar = true;
+  // note plugins setup
+  $.noty.defaults.theme = 'bootstrapTheme';
+  $.noty.defaults.timeout = 5000;
+  $.noty.defaults.progressBar = true;
 
-    var $mensagemElement = "<div class='row message-item' style='border-bottom: 1px solid #f5f5f5; margin-top: 15px;'>" +
-        "                    <div class='col-sm-8'>" +
-        "                        <input name='mensagemId[]' type='hidden'>" +
-        "                        <div class='form-group'>" +
-        "                            <label>Mensagem</label>" +
-        "                            <input name='mensagemTxt[]' type='text' class='form-control'>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                    <div class='col-sm-2'>" +
-        "                        <div class='form-group'>" +
-        "                            <label>Ordem</label>" +
-        "                            <select name='mensagemOrdem[]' class='form-control'>" +
-        "                                <option value='antes'>Antes</option>" +
-        "                                <option value='depois'>Depois</option>" +
-        "                            </select>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                    <div class='col-sm-1'>" +
-        "                        <div class='form-group'>" +
-        "                            <label>Pontos</label>" +
-        "                            <select name='pontos' class='form-control'>" +
-        "                                <option value='1'>1</option>" +
-        "                                <option value='2'>2</option>" +
-        "                                <option value='3'>3</option>" +
-        "                                <option value='4'>4</option>" +
-        "                                <option value='5'>5</option>" +
-        "                                <option value='6'>6</option>" +
-        "                                <option value='7'>7</option>" +
-        "                                <option value='8'>8</option>" +
-        "                                <option value='9'>9</option>" +
-        "                                <option value='10'>10</option>" +
-        "                                <option value='11'>11</option>" +
-        "                                <option value='12'>12</option>" +
-        "                                <option value='13'>13</option>" +
-        "                            </select>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                    <div class='col-sm-1'>" +
-        "                        <div class='form-group'>" +
-        "                            <span class='btn btn-danger message-delete' style=\"margin-top: 1.7em;\">" +
-        "                                <i class=\"fa fa-trash\" aria-hidden=\"true\"></i>" +
-        "                            </span>" +
-        "                        </div>" +
-        "                    </div>" +
-        "                </div>";
+  var $mensagemElement = "<div class='row message-item' style='border-bottom: 1px solid #f5f5f5; margin-top: 15px;'>" +
+    "                    <div class='col-sm-8'>" +
+    "                        <input name='mensagemId[]' type='hidden'>" +
+    "                        <div class='form-group'>" +
+    "                            <label>Mensagem</label>" +
+    "                            <input name='mensagemTxt[]' type='text' class='form-control mensagemTxt'>" +
+    "                        </div>" +
+    "                    </div>" +
+    "                    <div class='col-sm-2'>" +
+    "                        <div class='form-group'>" +
+    "                            <label>Ordem</label>" +
+    "                            <select name='mensagemOrdem[]' class='form-control'>" +
+    "                                <option value='antes'>Antes</option>" +
+    "                                <option value='depois'>Depois</option>" +
+    "                            </select>" +
+    "                        </div>" +
+    "                    </div>" +
+    "                    <div class='col-sm-1'>" +
+    "                        <div class='form-group'>" +
+    "                            <label>Pontos</label>" +
+    "                            <select name='pontos' class='form-control'>" +
+    "                                <option value='1'>1</option>" +
+    "                                <option value='2'>2</option>" +
+    "                                <option value='3'>3</option>" +
+    "                                <option value='4'>4</option>" +
+    "                                <option value='5'>5</option>" +
+    "                                <option value='6'>6</option>" +
+    "                                <option value='7'>7</option>" +
+    "                                <option value='8'>8</option>" +
+    "                                <option value='9'>9</option>" +
+    "                                <option value='10'>10</option>" +
+    "                                <option value='11'>11</option>" +
+    "                                <option value='12'>12</option>" +
+    "                                <option value='13'>13</option>" +
+    "                            </select>" +
+    "                        </div>" +
+    "                    </div>" +
+    "                    <div class='col-sm-1'>" +
+    "                        <div class='form-group'>" +
+    "                            <span class='btn btn-danger message-delete' style=\"margin-top: 1.7em;\">" +
+    "                                <i class=\"fa fa-trash\" aria-hidden=\"true\"></i>" +
+    "                            </span>" +
+    "                        </div>" +
+    "                    </div>" +
+    "                </div>";
 
-    function onMessageError(text, type) {
-        noty({
-            text: String(text),
-            type: String(type)
-        });
-    };
+  function onMessageError(text, type) {
+    noty({
+      text: String(text),
+      type: String(type)
+    });
+  };
 
-    function onMessageDeleteClick() {
-        if ($('.message-item').length !== 1) {
-            $(this).parents('.message-item').hide('slow', function () {
-                $(this).remove();
-            });
-        } else {
-            onMessageError("Você deve ao menos ter uma <b>mensagem</b>", 'error');
-        }
+  function onMessageDeleteClick() {
+    if ($('.message-item').length !== 1) {
+      $(this).parents('.message-item').hide('slow', function () {
+        $(this).remove();
+      });
+    } else {
+      onMessageError("Você deve ao menos ter uma <b>mensagem</b>", 'error');
     }
+  };
 
-    function onMessageAdd() {
-        $('#message-list').append($mensagemElement);
-        onActiveselectTwo();
-        $('.message-delete').click(onMessageDeleteClick);
-    };
+  function onMessageAdd() {
+    $('#message-list').append($mensagemElement);
+    onActiveselectTwo();
+    $('.message-delete').click(onMessageDeleteClick);
+    $('.mensagemTxt').keydown(onAutoComplete);
+  };
 
-    $('.message-add').click(onMessageAdd);
+  function onAutoComplete() {
+    $(".mensagemTxt").autocomplete({
+      source: function (request, response) {
+        $.getJSON("http://sparv-ufc.app/api/mensagems/?search=" + request.term, function (data) {
+          response($.map(data.data, function (value, key) {
+            return {
+              label: value.mensagemTxt,
+              value: key
+            };
+          }));
+        });
+      },
+      minLength: 2,
+      delay: 100
+    });
+  };
 
-    onMessageAdd();
+  $('.message-add').click(onMessageAdd);
+
+  onMessageAdd();
 
 });
