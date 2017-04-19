5da87e9113d0a5c4dc89e096cccbd82a8deaefb8
Luítame de Oliveira <luitamy@hotmail.com>
2017-04-19 11:45:44 +0000

    trabalhando com mustache templates na view

--- resources/views/fases/fields.blade.php
+++ resources/views/fases/fields.blade.php
@@ -1,5 +1,3 @@
-<style>
-</style>
 <div class="row">
     <div class="col-sm-3 card_image"></div>
 </div>
@@ -22,7 +20,8 @@
                 {!! Form::text('carta_id', null, ['id' => 'carta_id', 'class' => 'form-control', 'required']) !!}
             </div>
             <div class="col-sm-3">
-                <a style="margin-top: 2px;" href="#" class="btn btn-warning btn-sm random_card">Escolher carta aleatória</a>
+                <a style="margin-top: 2px;" href="#" class="btn btn-warning btn-sm random_card">Escolher carta
+                    aleatória</a>
             </div>
         </div>
     </div>
@@ -76,7 +75,7 @@
             <div class="box-footer" id="message-list" style="overflow: hidden;">
                 <div class='row message-item' id="message-nothing-item"
                      style='border-bottom: 1px solid #f5f5f5; margin-top: 15px;'>
-                    <div class="col-sm-12 bg-warning message-nothing-text">
+                    <div class="col-sm-12 bg-warning" style="padding: 2rem;">
                         Nenhuma mensagem adicionada à esta fase
                     </div>
                 </div>
@@ -95,12 +94,89 @@
         <a href="{!! route('fases.index') !!}" class="btn btn-default">Voltar</a>
     </div>
 </div>
+
+<template id="message-element">
+    <div class='row message-item' style='border-bottom: 1px solid #f5f5f5; margin-top: 15px;'>
+        <div class='col-sm-8'>
+            <div class='form-group'>
+                <label>Mensagem</label>
+                <input required name='mensagemTxt[]' type='text' class='form-control mensagemTxt'>
+                <input required name='mensagemId[]' type='hidden' class='form-control mensagemId'>
+            </div>
+        </div>
+        <div class='col-sm-2'>
+            <div class='form-group'>
+                <label>Ordem</label>
+                <select required name='mensagemOrdem[]' class='form-control'>
+                    <option value='antes'>Antes</option>
+                    <option value='depois'>Depois</option>
+                </select>
+            </div>
+        </div>
+        <div class='col-sm-1'>
+            <div class='form-group'>
+                <label>Pontos</label>
+                <select required name='mensagemPontos[]' class='form-control'>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
+                    <option value='6'>6</option>
+                    <option value='7'>7</option>
+                    <option value='8'>8</option>
+                    <option value='9'>9</option>
+                    <option value='10'>10</option>
+                    <option value='11'>11</option>
+                    <option value='12'>12</option>
+                    <option value='13'>13</option>
+                </select>
+            </div>
+        </div>
+        <div class='col-sm-1'>
+            <div class='form-group'>
+                <span class='btn btn-danger message-delete' style='margin-top: 1.7em;'>
+                    <i class='fa fa-trash' aria-hidden='true'></i>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<template id="message-nothing-item">
+    <div class='row message-item' id='message-nothing-item' style='border-bottom: 1px solid #f5f5f5; margin-top: 15px;'>
+        <div class='col-sm-12 bg-warning' style="padding: 2rem;">Nenhuma mensagem adicionada à esta fase</div>
+    </div>
+</template>
+
 @section('css')
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <style>
+        .ui-menu .ui-menu-item {
+            color: #444;
+            padding: 3px 0px;
+            border-bottom: 1px solid #ccc;
+        }
+
+        .ui-menu-item .ui-menu-item-wrapper:hover {
+            padding: 3px 6px;
+        }
+
+        .ui-state-active,
+        .ui-widget-content .ui-state-active,
+        .ui-widget-header .ui-state-active,
+        a.ui-button:active,
+        .ui-button:active,
+        .ui-button.ui-state-active:hover {
+            color: white;
+            background: #3C8DBC;
+            border: none;
+            padding: 3px 6px;
+        }
+    </style>
 @endsection
 @section('scripts')
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="{{url('js/noty/packaged/jquery.noty.packaged.js')}}"></script>
     <script src="{{url('js/dynamic-boxes.js')}}"></script>
     <script>
         $('i').popover({});
