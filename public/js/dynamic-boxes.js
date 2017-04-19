caa3fbe5a987d6f9111d1137194a52f4684c88d2
Luítame de Oliveira <luitamy@hotmail.com>
2017-04-19 11:46:13 +0000

    usando o mustache e comentando bugs

--- public/js/dynamic-boxes.js
+++ public/js/dynamic-boxes.js
@@ -1,102 +1,38 @@
 $(function () {
 
-  // note plugins setup
-  $.noty.defaults.theme = 'bootstrapTheme';
-  $.noty.defaults.timeout = 5000;
-  $.noty.defaults.progressBar = true;
-
-  var $mensagemElement = "<div class='row message-item' style='border-bottom: 1px solid #f5f5f5; margin-top: 15px;'>" +
-    "                    <div class='col-sm-8'>" +
-    "                        <div class='form-group'>" +
-    "                            <label>Mensagem</label>" +
-    "                            <input required name='mensagemTxt[]' type='text' class='form-control mensagemTxt'>" +
-    "                            <input required name='mensagemId[]' type='hidden' class='form-control mensagemId'>" +
-    "                        </div>" +
-    "                    </div>" +
-    "                    <div class='col-sm-2'>" +
-    "                        <div class='form-group'>" +
-    "                            <label>Ordem</label>" +
-    "                            <select required name='mensagemOrdem[]' class='form-control'>" +
-    "                                <option value='antes'>Antes</option>" +
-    "                                <option value='depois'>Depois</option>" +
-    "                            </select>" +
-    "                        </div>" +
-    "                    </div>" +
-    "                    <div class='col-sm-1'>" +
-    "                        <div class='form-group'>" +
-    "                            <label>Pontos</label>" +
-    "                            <select required name='mensagemPontos[]' class='form-control'>" +
-    "                                <option value='1'>1</option>" +
-    "                                <option value='2'>2</option>" +
-    "                                <option value='3'>3</option>" +
-    "                                <option value='4'>4</option>" +
-    "                                <option value='5'>5</option>" +
-    "                                <option value='6'>6</option>" +
-    "                                <option value='7'>7</option>" +
-    "                                <option value='8'>8</option>" +
-    "                                <option value='9'>9</option>" +
-    "                                <option value='10'>10</option>" +
-    "                                <option value='11'>11</option>" +
-    "                                <option value='12'>12</option>" +
-    "                                <option value='13'>13</option>" +
-    "                            </select>" +
-    "                        </div>" +
-    "                    </div>" +
-    "                    <div class='col-sm-1'>" +
-    "                        <div class='form-group'>" +
-    "                            <span class='btn btn-danger message-delete' style=\"margin-top: 1.7em;\">" +
-    "                                <i class=\"fa fa-trash\" aria-hidden=\"true\"></i>" +
-    "                            </span>" +
-    "                        </div>" +
-    "                    </div>" +
-    "                </div>";
-
-  var $messageNothingItem = "<div class='row message-item' id='message-nothing-item' style='border-bottom: 1px solid #f5f5f5; margin-top: 15px;'>" +
-    "                           <div class='col-sm-12 bg-warning message-nothing-text'>" +
-    "                               Nenhuma mensagem adicionada à esta fase" +
-    "                           </div>" +
-    "                        </div>";
-
-  function onMessageError(text, type) {
-    noty({
-      text: String(text),
-      type: String(type)
-    });
-  };
+  var $messageList = $('#message-list');
+  var $messageItem = $(".message-item");
+  var $mensagemElement = $('#message-element').html();
+  var $messageNothingItem = $('#message-nothing-item');
+  var $messageNothingItemTemplate = $('#message-nothing-item').html();
 
   function onMessageDeleteClick() {
 
-    if ($('.message-item').length !== 0) {
+    if ($messageItem.length !== 0) {
       $(this).parents('.message-item').hide('slow', function () {
         $(this).remove();
       });
     }
 
-    if ($('.message-item').length === 1) {
-      $('#message-list').fadeIn('slow', function () {
-        $(this).append($messageNothingItem);
-      });
-    }
+    // if ($messageItem.length === 1) {
+    //   $messageList.fadeIn('slow', function () {
+    //     $(this).append(Mustache.render($messageNothingItemTemplate));
+    //   });
+    // }
 
   };
 
   function onMessageAdd() {
-
     // removing message warning text in the add one message item
-    if ($('#message-nothing-item').length === 1) {
-      $('#message-nothing-item').hide('slow', function () {
+    if ($messageNothingItem.length === 1) {
+      $messageNothingItem.hide('slow', function () {
         $(this).remove();
       });
     }
-
-    $('#message-list').append($mensagemElement);
-
+    $messageList.append(Mustache.render($mensagemElement));
     onActiveselectTwo();
-
     $('.message-delete').click(onMessageDeleteClick);
-
     $('.mensagemTxt').keydown(onAutoComplete);
-
   };
 
   function onAutoComplete() {
@@ -122,16 +58,15 @@ $(function () {
 
   function onGetCard(number) {
     $.getJSON("http://sparv.app/api/cartas/" + number, function (data) {
-
       var card = data.data.nome;
-
       if ($(".card-thumb").length > 0) {
-        $('.card-thumb').remove();
+        $('.card-thumb').fadeOut('slow', function () {
+          $(this).remove();
+        });
       }
-
-      $(".card_image")
-        .append("<img src='http://sparv.app/images/cartas/" + card + ".png' class='card-thumb' style='max-height: 200px; border: 1px solid #3C8DBC;'/>");
-
+      setTimeout(function () {
+        $(".card_image").append("<img src='http://sparv.app/images/cartas/" + card + ".png' class='card-thumb' style='max-height: 200px; border: 1px solid #3C8DBC;'/>");
+      }, 500);
       $("#carta_id").val(number.toString());
     });
   }
@@ -167,6 +102,5 @@ $(function () {
         e.preventDefault();
       }
     });
-
 
 });
