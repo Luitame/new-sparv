cd25b96cdc0eaf82f101ce7ab921f9657afff341
Luítame de Oliveira <luitamy@hotmail.com>
2017-04-26 18:14:46 +0000

    configurando criteria

--- app/Repositories/PerguntaRepository.php
+++ app/Repositories/PerguntaRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Pergunta;
 use InfyOm\Generator\Common\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
 
 class PerguntaRepository extends BaseRepository
 {
@@ -11,8 +12,13 @@ class PerguntaRepository extends BaseRepository
      * @var array
      */
     protected $fieldSearchable = [
-        'perguntaTxt'
+        'perguntaTxt' => 'like'
     ];
+
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
 
     /**
      * Configure the Model
