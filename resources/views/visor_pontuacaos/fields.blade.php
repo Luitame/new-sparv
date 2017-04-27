297b5ec1ffc5f930353014904fee1b598b3a1370
Luítame de Oliveira <luitamy@hotmail.com>
2017-04-27 04:32:45 +0000

    alinhando campos no grid do bootstrap

--- resources/views/visor_pontuacaos/fields.blade.php
+++ resources/views/visor_pontuacaos/fields.blade.php
@@ -1,24 +1,15 @@
 <!-- Visibilidadeinicial Field -->
-
 <div class="row">
     <div class="form-group col-sm-2">
         {!! Form::label('visibilidadeInicial', 'Visibilidade Inicial:') !!}
-    </div>
-</div>
-<div class="row">
-    <div class="form-group col-sm-2">
         {!! Form::select('visibilidadeInicial', ['visivel' => 'Visível', 'invisivel' => 'Invisível'], null, ['class' => 'form-control']) !!}
     </div>
 </div>
 
-<!-- Mudandoem Field -->
+<!-- Mudando em Field -->
 <div class="row">
     <div class="form-group col-sm-2">
         {!! Form::label('mudandoEm', 'Mudando Em:') !!}
-    </div>
-</div>
-<div class="row">
-    <div class="form-group col-sm-1">
         {!! Form::text('mudandoEm', null, ['class' => 'form-control tempo', 'style' => 'padding:5px;']) !!}
     </div>
 </div>
