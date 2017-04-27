ae83a37291b503d4de99f039089858bda71adf2c
Luítame de Oliveira <luitamy@hotmail.com>
2017-04-27 05:10:11 +0000

    exibindo id da carta para possivel consulta

--- resources/views/cartas/grid.blade.php
+++ resources/views/cartas/grid.blade.php
@@ -1,11 +1,12 @@
 <div class="row">
     @foreach($cartas as $key => $carta)
-        <div class="col-md-2 portfolio-item">
+        <div class="col-md-2 portfolio-item text-center">
             <a href="{{route('cartas.show', [$carta->id])}}" data-toggle="tooltip" data-placement="top" title="Nome: {{$carta->nome}}, Número: {{$carta->numero}}, Cor: {{$carta->cor}} e Simbolo: {{$carta->simbolo}}.">
                 <img class="img-responsive"
                      src="{{url('images/cartas/'.$carta->nome.'.png')}}"
                      style="overflow: hidden; width: 199px; height: 257px; border: 1px solid #ccc; margin-top: 15px;"
                      alt="{{$carta->nome}}">
+                <span>#{{$carta->id}}</span>
             </a>
         </div>
     @endforeach
