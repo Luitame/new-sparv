8f053de0e9e96e2a732d730684c7b8b57a37c6df
Luítame de Oliveira <luitamy@hotmail.com>
2017-04-27 05:02:32 +0000

    setando mascara para os campos de tempo e criando placeholder

--- resources/views/regra_extras/fields.blade.php
+++ resources/views/regra_extras/fields.blade.php
@@ -2,7 +2,7 @@
 <div class="row">
     <div class="form-group col-sm-1">
         {!! Form::label('inicio', 'Início:') !!}
-        {!! Form::text('inicio', null, ['class' => 'form-control tempo', 'style' => 'padding:5px;']) !!}
+        {!! Form::text('inicio', null, ['class' => 'form-control tempo', 'placeholder' => '00:00:00', 'style' => 'padding:5px;']) !!}
     </div>
 </div>
 
@@ -10,7 +10,7 @@
 <div class="row">
     <div class="form-group col-sm-1">
         {!! Form::label('fim', 'Fim:') !!}
-        {!! Form::text('fim', null, ['class' => 'form-control tempo', 'style' => 'padding:5px;']) !!}
+        {!! Form::text('fim', null, ['class' => 'form-control tempo', 'placeholder' => '00:00:00', 'style' => 'padding:5px;']) !!}
     </div>
 </div>
 
@@ -18,7 +18,7 @@
 <div class="row">
     <div class="form-group col-sm-2">
         {!! Form::label('criterio', 'Critério/Loop:') !!}
-        {!! Form::text('criterio', null, ['class' => 'form-control']) !!}
+        {!! Form::text('criterio', null, ['class' => 'form-control', 'placeholder' => 'Segundo ou lado']) !!}
     </div>
 </div>
 
@@ -41,4 +41,4 @@
     </div>
 </div>
 
-{{--@include('vendor.mask.mask')--}}
+@include('layouts.mask.mask')
