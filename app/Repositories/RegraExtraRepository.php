3910261cf9a0982e0a9f0b0e592ce9246c70cd75
Luítame de Oliveira <luitamy@hotmail.com>
2017-05-03 19:36:02 -0300

    criacao de regra extra completo

--- app/Repositories/RegraExtraRepository.php
+++ app/Repositories/RegraExtraRepository.php
@@ -24,4 +24,50 @@ class RegraExtraRepository extends BaseRepository
     {
         return RegraExtra::class;
     }
+
+    /**
+     * @param array $attributes
+     */
+    public function createWithRelationships(array $attributes)
+    {
+        $mensagems = $attributes['mensagemId'] ?? '';
+        $perguntas = $attributes['perguntaId'] ?? '';
+
+        $regraExtra = $this->model->create($attributes);
+
+        if (!empty($mensagems)) {
+            foreach ($mensagems as $key => $mensagemId) {
+                $regraExtra->mensagems()->attach($mensagemId, ['ordem' => $attributes['mensagemOrdem'][$key], 'pontos' => $attributes['mensagemPontos'][$key]]);
+            }
+        }
+
+        if (!empty($perguntas)) {
+            foreach ($perguntas as $key => $perguntaId) {
+                $regraExtra->perguntas()->attach($perguntaId, ['ordem' => $attributes['mensagemOrdem'][$key], 'pontos' => $attributes['mensagemPontos'][$key]]);
+            }
+        }
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function updateWithRelationships(array $attributes, $id)
+    {
+        $mensagems = $attributes['mensagemId'] ?? '';
+        $perguntas = $attributes['perguntaId'] ?? '';
+
+        $regraExtra = $this->model->update($attributes, $id);
+
+        if (!empty($mensagems)) {
+            foreach ($mensagems as $key => $mensagemId) {
+                $regraExtra->mensagems()->sync($mensagemId, ['ordem' => $attributes['mensagemOrdem'][$key], 'pontos' => $attributes['mensagemPontos'][$key]]);
+            }
+        }
+
+        if (!empty($perguntas)) {
+            foreach ($perguntas as $key => $perguntaId) {
+                $regraExtra->perguntas()->sync($perguntaId, ['ordem' => $attributes['mensagemOrdem'][$key], 'pontos' => $attributes['mensagemPontos'][$key]]);
+            }
+        }
+    }
 }
