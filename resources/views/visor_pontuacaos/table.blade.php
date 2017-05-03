ce289dac544475d31a95acf8b1b8bf47db2a9886
Luítame de Oliveira <luitamy@hotmail.com>
2017-05-03 23:57:42 +0000

    informando quando não nenhum visor de pontuação cadastrado

--- resources/views/visor_pontuacaos/table.blade.php
+++ resources/views/visor_pontuacaos/table.blade.php
@@ -1,12 +1,12 @@
 <table class="table table-responsive table-hover" id="visorPontuacaos-table">
     <thead>
-        <th>#</th>
-        <th>Visibilidade Inicial</th>
-        <th>Mudando Em</th>
-        <th colspan="3">Ação</th>
+    <th>#</th>
+    <th>Visibilidade Inicial</th>
+    <th>Mudando Em</th>
+    <th colspan="3">Ação</th>
     </thead>
     <tbody>
-    @foreach($visorPontuacaos as $visorPontuacao)
+    @forelse($visorPontuacaos as $visorPontuacao)
         <tr>
             <td>{!! $visorPontuacao->id !!}</td>
             <td>{!! $visorPontuacao->visibilidadeInicial !!}</td>
@@ -14,13 +14,20 @@
             <td>
                 {!! Form::open(['route' => ['visorPontuacaos.destroy', $visorPontuacao->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('visorPontuacaos.show', [$visorPontuacao->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('visorPontuacaos.edit', [$visorPontuacao->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                    <a href="{!! route('visorPontuacaos.show', [$visorPontuacao->id]) !!}"
+                       class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                    <a href="{!! route('visorPontuacaos.edit', [$visorPontuacao->id]) !!}"
+                       class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Você tem certeza?')"]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
         </tr>
-    @endforeach
+    @empty
+        <tr>
+            <td colspan="6" class="text-center bg-warning text-warning">Nenhum <b>Visor de Pontos</b> cadastrado.
+            </td>
+        </tr>
+    @endforelse
     </tbody>
 </table>
