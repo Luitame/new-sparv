f4ead84e308f107a6189c8815f8bbbb192aeb53b
Luítame de Oliveira <luitamy@hotmail.com>
2017-05-03 21:05:12 -0300

    informando quando não houver nenhuma regra extra cadastrada

--- resources/views/regra_extras/table.blade.php
+++ resources/views/regra_extras/table.blade.php
@@ -1,14 +1,14 @@
 <table class="table table-responsive table-hover" id="regraExtras-table">
     <thead>
-        <th>#</th>
-        <th>Início</th>
-        <th>Fim</th>
-        <th>Critério</th>
-        <th>Pontos</th>
-        <th colspan="3">Ação</th>
+    <th>#</th>
+    <th>Início</th>
+    <th>Fim</th>
+    <th>Critério</th>
+    <th>Pontos</th>
+    <th colspan="3">Ação</th>
     </thead>
     <tbody>
-    @foreach($regraExtras as $regraExtra)
+    @forelse($regraExtras as $regraExtra)
         <tr>
             <td>{!! $regraExtra->id !!}</td>
             <td>{!! $regraExtra->inicio !!}</td>
@@ -18,13 +18,19 @@
             <td>
                 {!! Form::open(['route' => ['regraExtras.destroy', $regraExtra->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('regraExtras.show', [$regraExtra->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('regraExtras.edit', [$regraExtra->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                    <a href="{!! route('regraExtras.show', [$regraExtra->id]) !!}" class='btn btn-default btn-xs'><i
+                            class="glyphicon glyphicon-eye-open"></i></a>
+                    <a href="{!! route('regraExtras.edit', [$regraExtra->id]) !!}" class='btn btn-default btn-xs'><i
+                            class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Você tem certeza?')"]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
         </tr>
-    @endforeach
+    @empty
+        <tr>
+            <td colspan="8" class="text-center bg-warning text-warning">Nenhuma <b>Regra Extra</b> cadastrada.</td>
+        </tr>
+    @endforelse
     </tbody>
 </table>
